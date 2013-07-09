45d9169f21ce85622c77b38eddb35be042b89906
bharat <b@g.com>
2013-07-09 18:00:52 +0530

    plist modified , color and system level added

--- Jansampark/JSModel.m
+++ Jansampark/JSModel.m
@@ -8,9 +8,12 @@
 
 #import "JSModel.h"
 
-#define k00Color [UIColor colorWithRed:1 green:0.43 blue:0.34 alpha:1]
-#define k01Color [UIColor colorWithRed:1 green:0.8 blue:0.2 alpha:1]
-#define k02Color [UIColor colorWithRed:0.16 green:0.85 blue:1 alpha:1]
+#define k00Color [UIColor colorWithRed:0.91 green:0.41 blue:0.41 alpha:1]
+#define k01Color [UIColor colorWithRed:0.97 green:0.72 blue:0.33 alpha:1]
+#define k02Color [UIColor colorWithRed:0.38 green:0.59 blue:0.87 alpha:1]
+#define k03Color [UIColor colorWithRed:0.53 green:0.42 blue:0.79 alpha:1]
+#define k04Color [UIColor colorWithRed:0.55 green:0.79 blue:0.35 alpha:1]
+#define k05Color [UIColor clearColor];
 
 #define kSystemLevel0 @"Lack of infrastructure"
 #define kSystemLevel1 @"Lack of maintainance"
@@ -42,9 +45,17 @@ static JSModel *sharedModel = nil;
     case 2:
       return k02Color;
       break;
-      
+    case 3:
+      return k03Color;
+      break;
+    case 4:
+      return k04Color;
+      break;
+    case 5:
+      return k05Color;
+      break;
     default:
-      return [UIColor blackColor];
+      return [UIColor clearColor];
       break;
   }
 }
