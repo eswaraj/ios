4d69c02737398964f12a850fc8e3c872019ac02a
bharat <b@g.com>
2013-07-11 13:24:37 +0530

    Issue Summary vc added

---
+++ Jansampark/IssueSummaryVC.h
@@ -0,0 +1,13 @@
+//
+//  IssueSummaryVC.h
+//  Jansampark
+//
+//  Created by dev27 on 7/11/13.
+//  Copyright (c) 2013 Grappus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IssueSummaryVC : UIViewController
+
+@end
